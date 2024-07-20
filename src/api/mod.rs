8afcd4fdc407705b5c67b9163b923338f2f8c2c8
zuzuka28 <zuzuka28kavaii@gmail.com>
2024-07-20 12:13:03 +0300

    add cors

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -2,15 +2,24 @@ mod category;
 mod model;
 mod recipe;
 
+use axum::http::Method;
 use axum::{routing::get, Router};
+use tower_http::cors::{self, CorsLayer};
 
 use crate::state::AppState;
 
 pub fn new_api(state: AppState) -> Router {
-    Router::new().route("/ping", get(ping)).nest(
-        "/recipes",
-        recipe::build(state.clone()).nest("/categories", category::build(state.clone())),
-    )
+    let cors = CorsLayer::new()
+        .allow_methods([Method::GET, Method::POST, Method::PUT, Method::DELETE])
+        .allow_origin(cors::Any);
+
+    Router::new()
+        .route("/ping", get(ping))
+        .nest(
+            "/recipes",
+            recipe::build(state.clone()).nest("/categories", category::build(state.clone())),
+        )
+        .layer(cors)
 }
 
 async fn ping() -> &'static str {
